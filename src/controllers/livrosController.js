774e53d56258c5574c026d3ac4d5e6bcc2f639fa
lennon.rodrigues <lennon.rodrigues@tallos.com.br>
2023-01-25 18:16:21 -0300

    feat: Populando autores na busca de livros

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -4,21 +4,25 @@ import { ObjectId } from "mongoose";
 class LivroController {
 
   static listarlivros = (req, res) => {
-    livros.find((err, livros) => {
-      res.status(200).json(livros);
-    })
+    livros.find()
+      .populate('autor', 'nome')
+      .exec((err, livros) => {
+        res.status(200).json(livros);
+      })
   }
 
   static buscarLivro = (req, res) => {
     const id = req.params.id;
 
-    livros.findById(id, (err, livro) => {
-      if (!err) {
-        res.status(200).send(livro)
-      } else {
-        res.status(400).send({ message: `${err.message}: Não conseguimos encontrar o livro informado, verifique se os parâmetros estão corretos.` })
-      }
-    })
+    livros.findById(id)
+      .populate('autor', 'nome')
+      .exec((err, livro) => {
+        if (!err) {
+          res.status(200).send(livro)
+        } else {
+          res.status(400).send({ message: `${err.message}: Não conseguimos encontrar o livro informado, verifique se os parâmetros estão corretos.` })
+        }
+      })
   }
 
   static cadastrarLivro = (req, res) => {
@@ -56,7 +60,7 @@ class LivroController {
       if (err) {
         return res.status(500).send({ message: err.message })
       }
-      
+
       if (!livro) {
         return res.status(404).send({ message: "Item não encontrado, verifique se os parâmetros estão corretos." })
       }
--- src/models/Livro.js
+++ src/models/Livro.js
@@ -4,7 +4,7 @@ const livroSchema = new Schema(
   {
     id: { type: String },
     titulo: { type: String, required: true },
-    autor: { type: String, required: true },
+    autor: { type: Schema.Types.ObjectId, ref: 'autores', required: true },
     editora: { type: String, required: true },
     numeroPaginas: { type: Number }
   }
